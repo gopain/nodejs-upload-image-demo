57a0e10a9fa6e73053b98767ebbfba300a47100c
billfeller <billfeller@gmail.com>
2014-11-16 01:48:26 +0800

    how to use node.js to upload file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 nodejs-upload-image-demo
 ========================
+
+1. 初始化项目信息：npm init
+2. 修改package.json，配置启动命令：
+
+  "scripts": {
+      "start": "node index.js"
+  },
+
+3. 安装依赖模块：npm install formidable --save
+4. index.js, server.js, router.js, requestHandler.js
+
+参考：
+    http://www.nodebeginner.org/index-zh-cn.html#handling-file-uploads
+    https://github.com/billfeller/nodebeginner
+
---
+++ index.js
@@ -0,0 +1,11 @@
+var server = require('./server');
+var router = require('./router');
+var requestHandler = require('./requestHandler');
+var formidable = require('formidable'); // require路径搜索算法？？
+
+var handler = {};
+handler['/'] = requestHandler.start;
+handler['/start'] = requestHandler.start;
+handler['/upload'] = requestHandler.upload;
+handler['/show'] = requestHandler.show;
+server.start(router.route, handler);
